b3252d54c22bef7213b961f6864a97d282662916
Writing with Fire <liyuxuan17307287190@gmail.com>
2021-07-19 09:59:08 +0800

    修改了API库

--- src/main/java/net/writingwithfire/saintchapter/common/tile/TileMindStone.java
+++ src/main/java/net/writingwithfire/saintchapter/common/tile/TileMindStone.java
@@ -21,7 +21,7 @@ public class TileMindStone extends TileEntity implements ITickableTileEntity {
         if (world != null && world.isRemote) {
             if (timer == MAX_TIME) {
                 PlayerEntity playerEntity = world.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), 10, false);
-                // debug
+                // debug 我将心石的计时逻辑改到客户端运行了。。。
 //                TranslationTextComponent translationTextComponent = new TranslationTextComponent("message.hello_block.hello");
                 if (playerEntity != null) {
 //                    playerEntity.sendStatusMessage(translationTextComponent, false);
