bd4c3841db8b962b57d352416ac57fbd9166a6f5
Writing with Fire <liyuxuan17307287190@gmail.com>
2021-09-17 06:42:15 +0800

    完成梦花火的注册

--- src/main/java/net/writingwithfire/saintchapter/common/registry/deferred/gameobjects/RegistryBlocks.java
+++ src/main/java/net/writingwithfire/saintchapter/common/registry/deferred/gameobjects/RegistryBlocks.java
@@ -1,9 +1,11 @@
 package net.writingwithfire.saintchapter.common.registry.deferred.gameobjects;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.FireBlock;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.writingwithfire.saintchapter.common.block.BlockDreamFire;
 import net.writingwithfire.saintchapter.common.block.BlockImpregnatedStone;
 import net.writingwithfire.saintchapter.common.block.BlockMindStone;
 
@@ -12,4 +14,5 @@ public class RegistryBlocks {
 
     public static final RegistryObject<Block> IMPREGNATED_STONE = BLOCKS.register("impregnated_stone", BlockImpregnatedStone::new);
     public static final RegistryObject<Block> MIND_STONE = BLOCKS.register("mind_stone", BlockMindStone::new);
+    public static final RegistryObject<Block> DREAM_FIRE = BLOCKS.register("dream_fire", BlockDreamFire::new);
 }
