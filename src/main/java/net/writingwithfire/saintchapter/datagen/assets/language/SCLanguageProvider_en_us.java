d70f24faab342f7710fa7c1c72377655ed423204
Writing with Fire <liyuxuan17307287190@gmail.com>
2021-09-22 09:39:57 +0800

    更新了README

--- src/main/java/net/writingwithfire/saintchapter/datagen/assets/language/SCLanguageProvider_en_us.java
+++ src/main/java/net/writingwithfire/saintchapter/datagen/assets/language/SCLanguageProvider_en_us.java
@@ -12,29 +12,5 @@ public class SCLanguageProvider_en_us extends net.minecraftforge.common.data.Lan
 
     @Override
     protected void addTranslations() {
-        // 物品
-        add(RegistryItems.REVELATION_SCROLL.get(), "启示卷轴");
-        add(RegistryItems.MIND_CRYSTAL.get(), "远古意志结晶");
-        add(RegistryItems.DEBUG_AND_TEST_STICK.get(), "调试棒");
-
-        // 方块
-        add(RegistryBlocks.MIND_STONE.get(), "心石");
-        add(RegistryBlocks.IMPREGNATED_STONE.get(), "浸渍石块");
-        add(RegistryBlocks.DEBUG_AND_TEST.get(), "调试方块");
-        add(RegistryBlocks.DREAM_FIRE.get(), "梦花火");
-
-        // 组
-        add("itemGroup.saintchapter", "圣章");
-
-        //帕秋莉的书
-        String patchouliBookName = "patchouli_books.book_of_truth.";
-        add(patchouliBookName + "name", "真理之书");
-        add(patchouliBookName + "landing_text", "");
-        add(patchouliBookName + "subtitle", "我一直在寻找");
-
-        // 成就
-        String advancementPrefix = "advancement.saintchapter.";
-        add(advancementPrefix + "hear_me_heartbeat.title", "聆听我的心跳");
-        add(advancementPrefix + "hear_me_heartbeat.description", "通过心跳声找到并获取心石");
     }
 }
